384e971578beb22b92d798616f915d1e61ea71a7
SweetHomeConstruction <86429329+SweetHomeConstruction@users.noreply.github.com>
2021-07-10 13:10:43 +0545

    Update Form-handler.php

--- Form-handler.php
+++ Form-handler.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
               "Subject:$subject.\n".
               "User Message: $message.\n";
 
-$to = 'rujankhiuju@gmail.com';
+$to = 'swthomeconstruction1144@gmail.com';
 
 $headers = "From:$email_from \r\n";
 
